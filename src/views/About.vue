0d8a59db5a54392159e298c347d9cf94960092c9
antoinetto <fa-tonio@hotmail.fr>
2021-03-03 15:51:54 +0100

    premier test branche

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="login">
+
+    <h1>Login page</h1>
+    <form action="POST">
+      <label>username <input type="text" name="username" value=""/></label>
+      <label>password <input type="text" name="password" value=""/></label>
+      <button type="submit"> Connexion </button>
+    </form>
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+//import Bien from '@/components/Bien.vue';
+export default {
+  name: 'App',
+  components: {
+    //Bien
+  },
+  computed: {
+    ...mapGetters("immo", ["biens"])
+  },
+  
+  methods: {
+    ...mapActions("immo", ['load'])
+  },
+  
+  mounted() {
+            this.load();
+  }
+}
+</script>
+
+<style> 
+  form {
+    display:grid;
+    grid-template-columns:auto;
+  }
+  button {
+    width:100px;
+    margin-left:auto;
+    margin-right:auto;
+  }
+</style>
